4968acbf1c1ce04b817ad6e3e447021f8e8013a6
raissaQueirozF <raissaqfonseca09@gmail.com>
2024-10-31 11:26:52 -0300

    Criação de Loja e Produto

---
+++ Loja.java
@@ -0,0 +1,62 @@
+package loja;
+import java.util.ArrayList;
+
+public class Loja {
+    private  String Nome;
+    private  ArrayList<Produto> Produtos;
+    private  Integer TotalVendas;
+
+    public Loja(String nome) {
+        this.Nome = nome;
+        this.TotalVendas = 0;
+        this.Produtos = new ArrayList<>();
+    }
+
+    public void MostrarProdutos() {
+
+        if (Produtos == null) {
+            return;
+        }
+
+        for (int i = 0; i < Produtos.size(); i++) {
+
+            System.out.println(i + " - "  + Produtos.get(i).getNome() + ", R$" + Produtos.get(i).getPreco());
+        }
+    }
+
+    public void venderProduto(Produto oldProduto) {
+        if (oldProduto.getQuantidade() == 0) {
+            System.out.println("Produto Indisponível");
+            return;
+        }
+
+        oldProduto.setQuantidade(oldProduto.getQuantidade()-1);
+        TotalVendas++;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return Produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        Produtos = produtos;
+    }
+
+    public Integer getTotalVendas() {
+        return TotalVendas;
+    }
+
+    public void setTotalVendas(Integer totalVendas) {
+        TotalVendas = totalVendas;
+    }
+
+    
+}
---
+++ Produto.java
@@ -0,0 +1,47 @@
+package loja;
+public class Produto {
+    private String Nome;
+    private Float Preco;
+    private Integer Quantidade;
+    private Loja Loja;
+
+    public Produto(String nome, Float preco, Integer quantidade, Loja loja) {
+        this.Nome = nome;
+        this.Preco = preco;
+        this.Quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public Float getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(Float preco) {
+        Preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return Quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        Quantidade = quantidade;
+    }
+
+    public Loja getLoja() {
+        return Loja;
+    }
+
+    public void setLoja(Loja loja) {
+        Loja = loja;
+    }
+
+    
+}
